b0584a1b771ad157c6fee574d722b65e7315cfdf
Iordan Georgiev <iordan340@gmail.com>
2023-05-23 02:05:44 +0300

    Create car-budget.clj

---
+++ src/lein/car-budget.clj
@@ -0,0 +1,40 @@
+(ns lein-app.car-budget)
+;; --- SPECS ---
+; we have 3 different brands with different prices
+; we have a coupon with % discount
+; let the user input its budget and coupon
+; 1. print out info if the coupon is valid
+; 2. find budget match (with applied coupon discount if applicable)
+
+(def cars [
+           {:name "bmw" :price 60000}
+           {:name "ferarri" :price 100000}
+           {:name "fiat" :price 20000}])
+
+(def coupon {:code "my_coupon" :percentage 20})
+
+(defn carPrice
+  [price discount] (if (nil? discount)
+                      (float price)
+                      (float (- price (* price (/ discount 100)))))
+  )
+
+(defn getDiscount
+  [code]
+  (if (= (:code coupon) code)
+    (:percentage coupon))
+  )
+
+(defn main
+  [budget code]
+  (def discount (getDiscount code))
+  (if (nil? discount)
+    (println "COUPON NOT VALID\n")
+    (println "COUPON VALID\n"))
+  (doseq
+    [car cars]
+    (do
+       (def price (carPrice (:price car) discount))
+       (if (<= price budget)
+         (println (:name car) "\t" price  )))
+  ))
